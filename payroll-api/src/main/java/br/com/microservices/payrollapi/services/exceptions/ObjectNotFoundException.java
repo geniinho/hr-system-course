007b8f728f6bcfe50bdfec6d01053d542c25fc5e
Genivaldo Alves <geniinho@hotmail.com>
2022-07-14 09:47:39 -0300

    payroll api created

---
+++ payroll-api/src/main/java/br/com/microservices/payrollapi/services/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,7 @@
+package br.com.microservices.payrollapi.services.exceptions;
+
+public class ObjectNotFoundException extends RuntimeException{
+    public ObjectNotFoundException(String message) {
+        super(message);
+    }
+}
